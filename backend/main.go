3c97d4f70b1d9d40ae181a67f5532bd2b8b82ba8
amaxyza <116742308+amaxyza@users.noreply.github.com>
2025-07-12 16:14:39 -0400

    PostgreSQL instantiate top of main

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/amaxyza/shadro/controllers"
+	"github.com/amaxyza/shadro/services"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	err := services.Connect()
+	defer services.ClosePool()
+
+	if err != nil {
+		fmt.Errorf("couldn't connect to postgresql renderer db")
+		return
+	}
+
 	r := gin.Default()
 
 	// static files served under /static/*
@@ -18,6 +29,7 @@ func main() {
 	{
 		api.GET("/users", controllers.GetUsersHandler)
 		api.GET("/users/:id", controllers.GetUserWithID)
+		//api.GET("/programs")
 
 		api.POST("/login", controllers.PostLoginHandler)
 		api.POST("/signup", controllers.PostCreateUserHandler)
