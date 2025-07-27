70fdf96e05038653b2406bd323277cc8a26fd011
amaxyza <116742308+amaxyza@users.noreply.github.com>
2025-07-27 01:07:30 -0400

    Fixed signature of CreateProgram

--- backend/services/programs.go
+++ backend/services/programs.go
@@ -8,12 +8,10 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-func CreateProgram(author *models.User, name, source string) (*models.Shader, error) {
+func CreateProgram(owner_id int, name, source string) (*models.Shader, error) {
 	// Create shader entry in postgresql database
 	var program_id int
 	var created_at, updated_at time.Time
-
-	owner_id := author.ID
 
 	err := pool.QueryRow(
 		context.Background(),
@@ -23,7 +21,7 @@ func CreateProgram(author *models.User, name, source string) (*models.Shader, er
 			"programname": name,
 			"glslsource":  source,
 		},
-	).Scan(&program_id, created_at, updated_at)
+	).Scan(&program_id, &created_at, &updated_at)
 
 	if err != nil {
 		return nil, err
@@ -55,6 +53,42 @@ func GetProgram(program_id int) (models.Shader, error) {
 	shader := models.NewShader(id, owner_id, name, source, created_at, updated_at)
 
 	return shader, nil
+}
+
+func GetAllUserPrograms(user_id int) ([]models.Shader, error) {
+	rows, err := pool.Query(
+		context.Background(),
+		`SELECT * FROM program WHERE owner_id = @user_id`,
+		pgx.NamedArgs{"user_id": user_id},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var programs_slice []models.Shader
+
+	scans := []any{new(int), new(int), new(string), new(string), new(time.Time), new(time.Time)}
+	_, err = pgx.ForEachRow(rows, scans, func() error {
+		var id, owner_id int
+		var name, source string
+		var created_at, updated_at time.Time
+
+		err := rows.Scan(&id, &owner_id, &name, &source, &created_at, &updated_at)
+		if err != nil {
+			return err
+		}
+
+		programs_slice = append(programs_slice, models.NewShader(id, owner_id, name, source, created_at, updated_at))
+
+		return nil
+	})
+
+	if rows.Err() != nil {
+		return nil, rows.Err()
+	}
+
+	return programs_slice, nil
 }
 
 func UpdateProgram(shaderprogram models.Shader, name, source string) error {
